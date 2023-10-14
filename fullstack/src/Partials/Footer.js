c6e5ec63919da4e6ae556a934f6e7fa0ddec3860
Reena-hub <77442684+Reena-hub@users.noreply.github.com>
2023-10-14 15:19:22 +0530

    Create Footer.js

---
+++ fullstack/src/Partials/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <React.Fragment>
+                <footer className="d-flex justify-content-between backg p-3 bottom w-100">
+                    <p className="m-0 here"> 2023 &copy; YSquare </p>
+                    <p className="m-0 here"> Design & Develop by ThemesBrand </p>
+                </footer>
+        </React.Fragment>
+    );
+}
+
+export default Footer;
